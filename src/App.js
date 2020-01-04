a32e2c2159b18fecfc2aa843314909bab7618e1d
Adam Toczko <adamtoczko80@gmail.com>
2020-01-04 19:50:36 +0100

    created Footer component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Hero from './components/Hero';
 import OurCrew from './components/OurCrew';
 import Services from './components/Services';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -14,18 +15,8 @@ function App() {
       <OurCrew />
       <Services />
       <Contact />
+      <Footer />
      
-      <footer>
-        <div className="container">
-          <h4>&copy; Adam Toczko, 2019</h4>
-        </div>
-        <button className="social-icon">
-          <i className="fab fa-instagram"></i>
-        </button>
-        <button className="social-icon">
-          <i className="fab fa-facebook-square"></i>
-        </button>
-      </footer>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+    <footer>
+        <div className="container">
+          <h4>&copy; Adam Toczko, 2019</h4>
+        </div>
+        <button className="social-icon">
+          <i className="fab fa-instagram"></i>
+        </button>
+        <button className="social-icon">
+          <i className="fab fa-facebook-square"></i>
+        </button>
+      </footer>
+    )
+}
+
+export default Footer;
