eac2dd8029a5b7dda5752f60420bf85d3d114350
Adam Toczko <adamtoczko80@gmail.com>
2020-01-04 19:28:59 +0100

    created Hero component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App">
-    <Header/>
+    <Header />
+    <Hero />
     
-    <section className="landing-page">
-        <div className="landing-page-shadow">
-            <div className="container">
-                <div className="landing-page-text">
-                    <h1>We offer best quality services</h1>
-                    <h2>Check us out </h2>
-                    <button className="btn">Special offer</button>
-                </div>
-            </div>
-        </div>
-    </section>
     <section className="our-crew" id="our-crew">
         <div className="container">
             <h1>Our Team</h1>
---
+++ src/components/Hero.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Hero = () =>
+{
+return( 
+    <section className="landing-page">
+    <div className="landing-page-shadow">
+        <div className="container">
+            <div className="landing-page-text">
+                <h1>We offer best quality services</h1>
+                <h2>Check us out </h2>
+                <button className="btn">Special offer</button>
+            </div>
+        </div>
+    </div>
+</section>
+    )
+    }
+export default Hero;
