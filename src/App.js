08d4b1f5691f36afc5034107c9570b80f0e3d02e
Adam Toczko <adamtoczko80@gmail.com>
2020-01-06 13:34:41 +0100

    used same prop selectedLang in App.js and changed to pl -

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
     <div className="App">
       <Header />
       <Main>
-        <Hero />
+        <Hero selectedLang={'pl'} />
         <OurCrew />
         <Services />
         <Contact />
