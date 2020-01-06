7f076a8cdc4bf6864d73292b48350ead6838ca99
Adam Toczko <adamtoczko80@gmail.com>
2020-01-06 14:29:00 +0100

    created data structure for services

---
+++ src/data.js
@@ -0,0 +1,27 @@
+
+const services = [
+	{
+		serviceText: 'Service 1',
+		isNew: true,
+	},
+	{
+		serviceText: 'Service 2',
+		isNew: false,
+	},
+	{
+		serviceText: 'Service 3',
+		isNew: false,
+	},
+	{
+		serviceText: 'Service 4',
+		isNew: true,
+	},
+	{
+		serviceText: 'Service 5',
+		isNew: false,
+	},
+	{
+		serviceText: 'Service 6',
+		isNew: true,
+	},
+]
