c893a6e80e7e78230c897735ab148b640e5a2d9b
Adam Toczko <adamtoczko80@gmail.com>
2020-01-06 13:33:36 +0100

    added prop to Hero section allowing to choose language for h1

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
-const Hero = () => {
+const langs = {
+    'pl':"Oferujemy najwyższą jakość usług ", 
+    'en':"We offer best quality services "
+  }
+  
+
+const Hero = ({selectedLang='en'}) => {
     return (
         <section className="landing-page">
             <div className="landing-page-shadow">
                 <div className="container">
                     <div className="landing-page-text">
-                        <h1>We offer best quality services</h1>
+                        <h1>{langs[selectedLang]} </h1>
                         <h2>Check us out </h2>
                         <button className="btn">Special offer</button>
                     </div>
