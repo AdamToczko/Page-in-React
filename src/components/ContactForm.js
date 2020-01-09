a56aa483f39812eb5389e3c66f6dd7b99e5ea17f
Adam Toczko <adamtoczko80@gmail.com>
2020-01-09 08:18:17 +0100

    created ContactForm component

---
+++ src/components/ContactForm.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+
+class ContactForm extends React.Component {
+
+    state = {
+
+    }
+  
+  
+    render(){
+  
+    //   const { services, busy, error } = this.state
+    //   if(busy == true ) {
+    //     return (
+    //       <section className="offer" id="offer">
+    //        <div className="container">
+    //          <h1>Data is loading  </h1>
+    //        </div>
+    //      </section>
+    //    )
+    //   } else {
+    //       if(error) {
+    //         return(
+    //        <section className="offer" id="offer">
+    //        <div className="container">
+    //          <h1>Could not get data from server  </h1>
+    //        </div>
+    //      </section>)
+  
+    //       } else {
+              return (
+               <section className="contact" id="contact">
+                    <div className="container">
+
+        
+                </div>
+                </section>
+            )
+            }}
+  
+  
+  export default ContactForm;
