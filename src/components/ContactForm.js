6c73fc217127c99f191eb0c911a4c148e2868501
Adam Toczko <adamtoczko80@gmail.com>
2020-01-09 09:09:56 +0100

    prepared contact form outline

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -4,13 +4,19 @@ import React from 'react';
 class ContactForm extends React.Component {
 
     state = {
-
-    }
+		subject: 'hidden',
+		fullName: '',
+		email: '',
+		content: '',
+		subscription: '',
+		busy: false,
+		error: false,
+	}
   
   
     render(){
-  
-    //   const { services, busy, error } = this.state
+      
+      const { subject, fullName, email, content, subscription, busy, error } = this.state
     //   if(busy == true ) {
     //     return (
     //       <section className="offer" id="offer">
@@ -32,8 +38,60 @@ class ContactForm extends React.Component {
               return (
                <section className="contact" id="contact">
                     <div className="container">
-
-        
+                    <form onSubmit={this.onFormSubmit}>
+							<div>
+								<label>Choose subject</label>
+								<select
+									name='subject'
+								>
+									<option disabled hidden value="hidden">Wybierz temat</option>
+									<option>Subject 1</option>
+									<option>Subject 2</option>
+									<option>Subject 3</option>
+								</select>
+							</div>
+							<div>
+								<input
+								required
+									type='text'
+									name='fullName'
+									placeholder='Eneter your full name'
+								/>
+							</div>
+							<div>
+								<input
+								required
+									type='email'
+									placeholder='Eneter email'
+									name='email'
+		
+							
+								/>
+							</div>
+							<div>
+								<textarea
+									required
+									placeholder='Type your message'
+									name='content'
+									
+								/>
+							</div>
+							<div>
+								<label htmlFor='subscription'>
+									<input
+										id='subscription'
+										type='checkbox'
+										required
+										name='subscription'
+									
+									/>
+									Subscribe
+								</label>
+							</div>
+							<div>
+								<button>Send</button>
+							</div>
+                        </form>
                 </div>
                 </section>
             )
