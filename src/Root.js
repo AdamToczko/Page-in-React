95f3cef7947c29f3b450ba15aa6d75b498b4d442
Adam Toczko <adamtoczko80@gmail.com>
2020-01-10 11:48:18 +0100

    created router with navigation now working and sending to each component

---
+++ src/Root.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import App from "./App";
+import Header  from "./components/Header";
+import Footer  from "./components/Footer";
+import OurCrew from "./components/OurCrew";
+import Contact from "./components/Contact";
+import Services from "./components/Services";
+import Hero from "./components/Hero";
+
+const Root = () => {
+    return (
+<Router>
+      <Header />
+      
+        <Switch>
+          <Route exact path="/" component={Hero} />
+          <Route exact path="/our-crew" component={OurCrew} />
+          <Route exact path="/offer" component={Services} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+        <Footer />
+    </Router>
+);
+};
+
+export default Root;
