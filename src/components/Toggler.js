b44065ee3a24c6609a6d2bae9454aac07d8167db
Adam Toczko <adamtoczko80@gmail.com>
2020-01-20 10:28:57 +0100

    added chevron icon to togglers

--- src/components/Toggler.js
+++ src/components/Toggler.js
@@ -20,11 +20,12 @@ class Toggler extends React.Component {
     render() {
         const {isShowed} = this.state
         const {children, title} = this.props
+        const icon = isShowed ? <i class="fas fa-chevron-up"></i> : <i class="fas fa-chevron-down"></i>
 
         return (
             	<div style={{margin:20, width: 200, boxShadow:'3px 3px 5px rgba(0,0,0,0.5)'}}>
 				    <a  onClick={this.toggle}>
-					    <h2 style={{color: 'gold', marginBottom: 10}}>{title} - click me </h2>
+					    <h2 style={{color: 'gold', marginBottom: 10}}>{title} - click me {icon}</h2>
                     </a>
                    
                  {isShowed &&   <div >
