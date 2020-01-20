1d35f6ccb186dc681778953dd34105d8629d2628
Adam Toczko <adamtoczko80@gmail.com>
2020-01-20 11:00:54 +0100

    refactor to remove errors from console

--- src/components/Toggler.js
+++ src/components/Toggler.js
@@ -3,24 +3,22 @@ import React from 'react'
 class Toggler extends React.Component {
 
     state = {
-        isShowed: false
+        isShowen: false
     }
 
     toggle = () => {
-        const {isShowed } = this.state
+        const {isShowen } = this.state
         this.setState({
-            isShowed: !isShowed
+            isShowen: !isShowen
         })
     }
 
-    // style={{margin:20, width: 200, boxShadow:'3px 3px 5px rgba(0,0,0,0.5)'}}
-    // style={{ display:'block', background: '#ddd', padding:10 }}
-    // style={{ background: '#eee', marginTop: 30 }}
+    
 
     render() {
-        const {isShowed} = this.state
+        const {isShowen} = this.state
         const {children, title} = this.props
-        const icon = isShowed ? <i class="fas fa-chevron-up"></i> : <i class="fas fa-chevron-down"></i>
+        const icon = isShowen ? <i className="fas fa-chevron-up"></i> : <i className="fas fa-chevron-down"></i>
 
         return (
             	<div style={{margin:20, width: 200, boxShadow:'3px 3px 5px rgba(0,0,0,0.5)'}}>
@@ -28,8 +26,8 @@ class Toggler extends React.Component {
 					    <h2 style={{color: 'gold', marginBottom: 10}}>{title} - click me {icon}</h2>
                     </a>
                    
-                 <div className={['animated', isShowed ? 'fadeIn' : 'fadeOut'].join(' ')} >
-                    <h5>{children}</h5>
+                 <div className={['animated', isShowen ? 'fadeIn' : 'fadeOut'].join(' ')} >
+                    {children}
                                 </div>
 			    </div>
         )
