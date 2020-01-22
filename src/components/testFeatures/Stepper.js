d8aae3f4824b68510be9225bdf3472d860479f22
Adam Toczko <adamtoczko80@gmail.com>
2020-01-22 10:15:48 +0100

    created method to change steps and connected with clicking buttons

--- src/components/testFeatures/Stepper.js
+++ src/components/testFeatures/Stepper.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Button } from "semantic-ui-react";
 
-const Steps = ({title}) => {
-    return (<span style={{marginLeft:20}}><Button>{title}</Button></span>)
+const Steps = ({ title, onChangeStep}) => {
+    return (<span style={{marginLeft:20}}><Button onClick={onChangeStep}>{title}</Button></span>)
 }
 
 
@@ -10,6 +10,13 @@ class Stepper extends React.Component {
 
     state = {
         actualStep: 0
+    }
+
+    setStep = (index) => {
+        
+        this.setState ({
+            actualStep: index
+        })
     }
 
     render() {
@@ -21,13 +28,18 @@ class Stepper extends React.Component {
         return (
 			<div style={{margin:20,  boxShadow:'3px 3px 5px rgba(11,100,0,0.5)'}}>
                 <div style={{ display:'block', background: '#fff', padding:10 }} >
-                    {children.map((child, key)=>{
-                        return (<Steps key={key} title={child.props.title} />)
+                    {children.map((child, index)=>{
+                        return (<Steps key={index} title={child.props.title} 
+                        onChangeStep={()=>{
+                            this.setStep(index)
+                        }
+                        }
+                        />)
                     }
                     )}
                 </div>    
                 <div style={{ display:'block', background: '#eee', padding:10 }} >
-                   {actualChild.props.title}
+                   {actualChild.props.children}
                 </div>    
 			</div>
 		)
