282b8f4ba097582b56e1a2eafb27f292cb9fb3e2
Adam Toczko <adamtoczko80@gmail.com>
2020-01-22 09:29:07 +0100

    created Stepper component general frame

---
+++ src/components/testFeatures/Stepper.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+class Stepper extends React.Component {
+
+    state = {
+        actualStep: 0
+    }
+
+    render() {
+        return (
+			<div style={{margin:20,  boxShadow:'3px 3px 5px rgba(0,0,0,0.5)'}}>
+                <div style={{ display:'block', background: '#ddd', padding:10 }} >
+                    
+                </div>    
+                <div style={{ display:'block', background: '#eee', padding:10 }} >
+                   
+                </div>    
+			</div>
+		)
+	}
+}
+
+
+export default Stepper
