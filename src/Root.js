0ff2e97b271383cb72716163bbb957a7f029cb06
Adam Toczko <adamtoczko80@gmail.com>
2020-02-04 11:21:37 +0100

    passed key from Route to CSS transition

--- src/Root.js
+++ src/Root.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import App from "./App";
-import Header  from "./components/Header";
-import Footer  from "./components/Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import OurCrew from "./components/OurCrew";
 import Contact from "./components/Contact";
 import Services from "./components/Services";
@@ -12,25 +12,28 @@ import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 const Root = () => {
     return (
-<Router>
-      <Header />
-        <TransitionGroup>
-            <CSSTransition
-              timeout={500}
-              classNames="fade"
-            >
-                <Switch>
-                <Route exact path="/" component={App} />
-                <Route exact path="/our-crew" component={OurCrew} />
-                <Route exact path="/offer" component={Services} />
-                <Route exact path="/contact" component={Contact} />
-                <Route exact path="/features" component={Features} />
-                </Switch>
-            </CSSTransition>
-        </TransitionGroup>
-        <Footer />
-    </Router>
-);
+        <Router>
+            <Header />
+            <Route render={({location}) => (
+                <TransitionGroup>
+                    <CSSTransition
+                    key={location.key}
+                    timeout={450}
+                    classNames="fade"
+                    >
+                        <Switch>
+                            <Route exact path="/" component={App} />
+                            <Route exact path="/our-crew" component={OurCrew} />
+                            <Route exact path="/offer" component={Services} />
+                            <Route exact path="/contact" component={Contact} />
+                            <Route exact path="/features" component={Features} />
+                        </Switch>
+                    </CSSTransition>
+                </TransitionGroup>
+            )} />
+            <Footer />
+        </Router>
+    );
 };
 
 export default Root;
